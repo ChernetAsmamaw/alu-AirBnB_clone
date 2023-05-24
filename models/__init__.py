a1371dd9b6b115ef336c8756a8fde3e1d633aa59
BevilMulor <b.mulor@alustudent.com>
2023-05-24 13:14:10 -0700

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-"""
-__init__ dunder method for the models directory
-"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/env python3
 
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
