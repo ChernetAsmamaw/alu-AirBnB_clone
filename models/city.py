4eade67e14db11c81e92a33d0c76ce86d56cbdda
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-05-25 18:01:44 +0000

    AirBnB clone

--- models/city.py
+++ models/city.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""Class City that inherits from the BaseModel class"""
-
-from models import BaseModel
-
-
-class City(BaseModel):
-    """City class"""
-
-    state_id = ""
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-=======
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
@@ -23,4 +7,3 @@ class City(BaseModel):
     """City class that inherits from BaseModel"""
     state_id = ""
     name = ""
->>>>>>> cc1bd00f1ca76611591882c0c790a7e0e53b17b7
