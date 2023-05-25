7ec983310c46880d7abc2861228844448924683d
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-05-25 21:35:45 +0300

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ from os.path import exists
 class FileStorage:
 
     # Private class attributes
-    # __ means they are private and shouldn't be accessed form outside the class
+    # __ means they're private & shouldn't be accessed outside the class
     # __file_path: string is the path to the JSON file
     # __objects: dictionary will store all objects by <class name>.id
     #            example: to store a BaseModel object with id=12121212,
@@ -39,29 +39,30 @@ class FileStorage:
         file_name = FileStorage.__file_path
         objects = FileStorage.__objects
 
-        # We convert the value(not the key) of each object to a readable string format
-        # Call on the to_dict() method from BaseModel class to convert EACH object to
-        #    a more understandable dictionary with string values and save them all in
-        #    a dictionary, so that they can be esaly parsed by
+        # We convert the value(not key) of each object to readable string format
+        # Call the to_dict() method from BaseModel to convert EACH object to
+        #    a more understandable dictionary with string values,
+        #    and save all in a dictionary, so they can be esaly parsed by
         #    programing languges for extraction. A dict inside a dict.
         # We can also write the below 3 lines of code in 1 line through
         # dictionary comprehension: -
-        #            obj_dict = {key: value.to_dict for key, value in objects.items()}
+        #   obj_dict = {key: value.to_dict for key, value in objects.items()}
 
         obj_dict = {}
         for key, value in objects.items():
             obj_dict[key] = value.to_dict()
 
-        # We need to open the file in write mode and encode(utf-8) then convert the
-        #   dictionary to a JSON string and saves it to the file
-        # The dump() function takes two arguments: the dictionary to be converted to
-        #   JSON and the file to save it to
+        # We need to open the file in write mode and encode(utf-8) then
+        # convert the dictionary to a JSON string and saves it to the file
+        # dump() function takes two arguments: 1. the dictionary to be
+        # converted to JSON and 2. the file to save it to
         # file_name is the path to the JSON file
         with open(file_name, mode="w", encoding="utf-8") as f:
             dump(obj_dict, f)
 
-    # Deserializes the JSON file to __objects. The reload() method will convert the JSON
-    #   string back to the dictionary __objects
+    # Deserializes
+    # JSON file to __objects. The reload() method will convert the JSON
+    #       string back to the dictionary __objects
 
     def reload(self):
 
@@ -79,23 +80,23 @@ class FileStorage:
         #   don't do anything if it doesn't exist
         if exists(file_name):
             # Here we open the file but since we are only reading from it,
-            #   we don't need to specify a mode or encoding(because it's the default)
+            # We don't need to specify a mode or encode(cuz it's the default)
             with open(file_name) as f:
                 obj_dict = load(f)
                 # print(obj_dict)
-        # At this point we are able to print the JSON file saved but are still in a
-        #   dictionary form and need to be converted to object form
+        # At this point, we're able to print the JSON file but are still
+        # in a dictionary form and need to be converted to object form
 
             for value in obj_dict.values():
-                # This gets the class name of the object and the eval() function makes sure
-                #   the class type is "type" and not a string (type is a built-in function)
+                # This gets class name of the object & the eval() function makes sure
+                #   the class type is "type" and not a string or something else
                 class_name = eval(value["__class__"])
                 del value["__class__"]
-                # Now we want to create a new object from the dictionary values by passing
-                #   them as keyword arguments to the BaseModel, user, or any other class
-                # Because both in the classes and here we have objects changed to to_dict
+                # Now we want to create a new object from the dictionary values
+                # by passing them as keyword arguments to the BaseModel, user...etc
+                # Cuz both in the classes & here we have objects changed to to_dict
                 #   so when passed through ** we get new objects
                 obj = class_name(**value)
-                # Here we call on the new method from above because we want to pass each
-                #   newly iterated object to __objects
+                # Here we call on the new method from above because we want to
+                # pass each newly iterated object to __objects
                 self.new(obj)
